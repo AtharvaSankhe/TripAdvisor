d890eaa2da7b775f260c48a5b965e3002ada61fd
AtharvaSankhe <atharvasankheer@gmail.com>
2025-03-03 17:16:30 +0530

    https

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import fs from "fs";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    https: {
+      key: fs.readFileSync("localhost-key.pem"),
+      cert: fs.readFileSync("localhost-cert.pem"),
+    },
+    host: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
